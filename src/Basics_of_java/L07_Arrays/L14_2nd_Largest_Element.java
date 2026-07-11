7b9239111ac3b19c6e8568d3f2ee8962a60c00fd
Piyush <piyushchhabra2007@gmail.com>
2026-07-11 19:31:03 +0530

    2nd Max Element

---
+++ src/Basics_of_java/L07_Arrays/L14_2nd_Largest_Element.java
@@ -0,0 +1,30 @@
+package Basics_of_java.L07_Arrays;
+
+import java.util.Scanner;
+
+public class L14_2nd_Largest_Element {
+    static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter Size of Array: ");
+        int n = sc.nextInt();
+        int arr[] = new int[n];
+        int i;
+        System.out.println("Enter Elements of array: ");
+        for (i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+        int max = arr[0];
+        for (i = 1; i < n; i++) {
+            if (arr[i] > max) {
+                max = arr[i];
+            }
+        }
+        int secondmax = Integer.MIN_VALUE;
+        for (i = 0; i < n; i++) {
+            if (arr[i] != max && arr[i] > secondmax) {
+                secondmax = arr[i];
+            }
+        }
+        System.out.println("Second Max Element: " + secondmax);
+    }
+}
