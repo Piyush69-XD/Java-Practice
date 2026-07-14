39d569321bd215023a4b31fe9702bb6da5d25176
Piyush <piyushchhabra2007@gmail.com>
2026-07-14 15:06:13 +0530

    Basics of Objects and Classes

---
+++ src/Basics_of_java/L09_Objects_and_Classes/L01_Basic/Main.java
@@ -0,0 +1,13 @@
+package Basics_of_java.L09_Objects_and_Classes.L01_Basic;
+
+public class Main {
+    static void main(String[] args) {
+        Student s1 = new Student();
+        s1.name = "Piyush";
+        s1.age = 19;
+        s1.roll_no = 2510991258L;
+
+        s1.print();
+
+    }
+}
---
+++ src/Basics_of_java/L09_Objects_and_Classes/L01_Basic/Student.java
@@ -0,0 +1,13 @@
+package Basics_of_java.L09_Objects_and_Classes.L01_Basic;
+
+public class Student {
+    String name;
+    int age;
+    long roll_no;
+
+    void print(){
+        System.out.println("Name: "+name);
+        System.out.println("Age: "+age);
+        System.out.println("Roll Number: "+roll_no);
+    }
+}
