45d1ce720cca3d130886aecde41d014ec47ca9b1
Piyush <piyushchhabra2007@gmail.com>
2026-07-16 09:50:10 +0530

    Count frequency of characters in a string

---
+++ src/Basics_of_java/L08_Strings/L13_Count_Frequency.java
@@ -0,0 +1,31 @@
+package Basics_of_java.L08_Strings;
+import java.util.Scanner;
+public class L13_Count_Frequency {
+    static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter Your String: ");
+        String s1 = sc.nextLine();
+        for(int i=0 ; i<s1.length();i++){
+            if(s1.charAt(i)==' '){
+                continue;
+            }
+            boolean visited = false;
+            for(int j=0;j<i;j++){
+                if(s1.charAt(i)==s1.charAt(j)){
+                    visited = true;
+                    break;
+                }
+            }
+            if(visited){
+                continue;
+            }
+            int count = 1;
+            for(int k = i+1;k<s1.length();k++){
+                if(s1.charAt(i)==s1.charAt(k)){
+                    count++;
+                }
+            }
+            System.out.println("Frequency of "+s1.charAt(i)+" = "+count);
+        }
+    }
+}
