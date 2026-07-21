389edef672b43cebffd00ca93307bb81d2a25259
Piyush <piyushchhabra2007@gmail.com>
2026-07-21 20:49:47 +0530

    Removing White Spaces from String

---
+++ src/Basics_of_java/L08_Strings/L31_Remove_Whitespaces.java
@@ -0,0 +1,12 @@
+package Basics_of_java.L08_Strings;
+import java.util.Scanner;
+public class L31_Remove_Whitespaces {
+    static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter String: ");
+        String s1 = sc.nextLine();
+
+        String s2 = s1.trim();
+        System.out.println("Modified String: "+s2);
+    }
+}
